540d0ba15c371d7d8c52df9f9e403c56e5ea0f56
Chatchawan Taja <tutorialgitcmtc@gmail.com>
2026-08-15 14:30:23 +0700

    f2

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function processPayment(price: number, qty: number) {
 
     console.log(`ยอดที่ต้องชำระทั้งหมด: ${grandTotal} บาท`);
 }
+
+function processPayment_V2(price: number, qty: number) {
+    if (price <= 0 || qty <= 0) {
+        console.log("ข้อมูลไม่ถูกต้อง");
+        return;
+    }
+
+    const subTotal = price * qty;
+    const fee = subTotal * 0.05;
+    const grandTotal = subTotal + fee;
+
+    console.log(`ยอดที่ต้องชำระทั้งหมด: ${grandTotal} บาท`);
+}
